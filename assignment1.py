0375616577460ba8c2d5b7302eac06dcac68f58b
Alex Lynne <jak4532@hotmail.com>
2020-08-06 11:13:26 +0800

    Add Menu

--- assignment1.py
+++ assignment1.py
@@ -1,13 +1,21 @@
 """
-Replace the contents of this module docstring with your own details
-Name:
-Date started:
+Name: Alexander James Hing Chong Lynne
+Date started: 08/06/2020
 GitHub URL:
+
 """
 
+MENU = print("Menu: \nL - List places \nA - Add new place \nQ - Quit")
 
 def main():
     print("Travel Tracker 1.0 - by <Your Name>")
+
+    MENU
+
+    menu_choice = input(">>> ").upper()
+    while menu_choice not in ["L", "A", "Q"]:
+        print("Invalid Menu Choice")
+        menu_choice = input(">>> ").upper()
 
 
 if __name__ == '__main__':
