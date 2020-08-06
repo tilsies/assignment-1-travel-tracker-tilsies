68bbd2ffba95356ca54431eb2221eff7dadcb496
Alex Lynne <jak4532@hotmail.com>
2020-08-06 11:37:52 +0800

    Add open file & count_places function

--- assignment1.py
+++ assignment1.py
@@ -5,12 +5,23 @@ GitHub URL:
 
 """
 
-MENU = print("Menu: \nL - List places \nA - Add new place \nQ - Quit")
+MENU = ("Menu: \nL - List places \nA - Add new place \nQ - Quit")
+
+def count_places(file):
+    num_places = 0
+    for row in file:
+        num_places += 1
+    return num_places
+
+
 
 def main():
-    print("Travel Tracker 1.0 - by <Your Name>")
+    print("Travel Tracker 1.0 - by Alexander Lynne")
+    in_file = open("places.csv", "r")
+    initial_num_places = count_places(in_file)
+    print("{0} places loaded from places.csv".format(initial_num_places))
 
-    MENU
+    print(MENU)
 
     menu_choice = input(">>> ").upper()
     while menu_choice not in ["L", "A", "Q"]:
