3cb170c711ca63b3a3ec6f00afd9f31a7efdad94
Alex Lynne <jak4532@hotmail.com>
2020-08-06 15:15:40 +0800

    Implement Menu choice = L Add count_unvisited

--- assignment1.py
+++ assignment1.py
@@ -28,15 +28,16 @@ def display_data(data):
         index += 1
         print("{5:1}{0}. {1:10} in {2:15} priority {3:>3}".format(index, *details))
 
+def count_unvisited(data):
+    count = sum(n.count("n") for n in data)
+    return count
+
+
 def main():
     print("Travel Tracker 1.0 - by Alexander Lynne")
     data = get_data()
     print("{0} places loaded from {1}".format(len(data), FILENAME))
-    print(data)  # TODO DELETE AFTER TESTING
-    display_data(data) #TODO MOVE TO MENU "L"
 
-
-    """
     print(MENU)
 
     menu_choice = input(">>> ").upper()
@@ -46,9 +47,17 @@ def main():
 
     while menu_choice in ["L", "A", "Q"]:
         if menu_choice == "L":
+            display_data(data)
+            num_unvisited = count_unvisited(data)
+            print("{0} places. You still want to visit {1} places.".format(len(data), num_unvisited))
+
+        else:
             continue
+
+        menu_choice = input(">>> ").upper()
+
             
-    """
+
 
 
 
